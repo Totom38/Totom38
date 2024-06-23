c94808bbb80f5a94823f6046f0827f1a2aa6610f
Tom <tom.gilgenkrantz@gmail.com>
2024-06-23 21:16:44 +0200

    Votre message de commit

--- scripts.js
+++ scripts.js
@@ -19,3 +19,21 @@ function closeAllPopups() {
         popup.style.display = 'none';
     });
 }
+
+function showOrigamiPopup(imageId) {
+    document.getElementById('origami-popup-' + imageId).style.display = 'flex';
+}
+
+function closeOrigamiPopup(event) {
+    if (event.target.classList.contains('origami-popup')) {
+        event.target.style.display = 'none';
+    }
+}
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape') {
+        document.querySelectorAll('.origami-popup').forEach(function(popup) {
+            popup.style.display = 'none';
+        });
+    }
+});
